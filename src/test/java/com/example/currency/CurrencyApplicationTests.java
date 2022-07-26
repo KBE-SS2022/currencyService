77ee6c5748d8c0b639cd36809ada5860f4753acd
EddeCCC <eduard.schander@gmx.de>
2022-07-27 01:28:25 +0200

    fix tests

--- src/test/java/com/example/currency/CurrencyApplicationTests.java
+++ src/test/java/com/example/currency/CurrencyApplicationTests.java
@@ -3,11 +3,9 @@ package com.example.currency;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CurrencyApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
